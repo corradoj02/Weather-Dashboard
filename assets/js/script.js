48349749d63a1b2fea54c857f14ba1fbb283124a
corradoj02 <josephcorrado97@gmail.com>
2023-02-01 15:57:05 -0500

    [+]Added base files and boiler plate + API Key

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var APIKey = "5c6c4a0c77be5bc599a1ce2c66810feb";
+var lat;
+var long;
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&appid=" + APIKey;
+
