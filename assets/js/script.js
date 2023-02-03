56107420798d60bbb2b4182885ad08ed296e0445
corradoj02 <josephcorrado97@gmail.com>
2023-02-03 10:07:57 -0500

    [+]Added some variable values and function names

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,34 +19,46 @@ var searchResults =
     };
     
 
-var getInput = () => {
-    var userInput = $('#userInput').html
+
+function getCity() {
+    return JSON.parse(localStorage.getItem('citySearch'))
 }
+
     
-    var getLat = () => {
-        localStorage.getItem(locationSearch.lat)
+    // var getLat = () => {
+    //     localStorage.getItem(locationSearch.lat)
+    // }
+    
+    
+    
+    // function that takes user input from city search, and adds them to the query 
+    function getCoord() {
+        var locationSearch = 
+        {
+        city: city,
+        lat: 0,
+        lon: 0,
+     }
+
+     locationSearch.city = $('#citySearch').val();
+
+     var geocode = "http://api.openweathermap.org/geo/1.0/direct?q=" + locationSearch.city + "&limit=5&appid=" + APIKey;
+
+     console.log(geocode);
     }
     
-    
-    
-    // function that takes user input from city search, and adds them to the query {
-    // var locationSearch = 
-    //     {
-    //     city: city,
-    //     lat: 0,
-    //     lon: 0,
-    //  }
 //     sets input as locationSearch.city variable
 //     localStorage.setItem("locationSearch", JSON.stringify(locationSearch))
 // }
 
 //    function uses city variable as search criteria for Geocoding API - then saves the lon and lat value to respective variables in locationSearch object
 //    var city = JSON.parse(localStorage.getItem(locationSearch.city));
-//    var geocode = "http://api.openweathermap.org/geo/1.0/direct?q=" + locationSearch.city + "&limit=5&appid=" + APIKey;
-//
+//    
+//      
 
 
 // function that takes parameters of previous function, and 
 
 
 // event listener that listens for user click on city search, then starts the function to get city coordinates from geocoding API
+$('#search-btn').on('click', search);
